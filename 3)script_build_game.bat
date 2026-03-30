8b49b276374604665d85a088de571a97e4b2eaf7
hkmodd <64160552+hkmodd@users.noreply.github.com>
2026-03-30 16:36:47 +0200

    Add log

--- 3)script_build_game.bat
+++ 3)script_build_game.bat
@@ -1,3 +1,8 @@
-cmake --build build_clang --config Release -j 14
+@echo off
+if not exist logs mkdir logs
+echo Building PS2xRunner... Logging to logs\runner_build_output.log
+echo.
+
+cmake --build build_clang --config Release -j 14 > logs\build_runner.log 2>&1
 echo "Finito"
 pause
